681811f3f0232646873ee376a45f2ca4bff22055
Andrew Sikes <40582475+umyemri@users.noreply.github.com>
2020-05-18 01:00:22 -0700

    Create basic.sh

---
+++ basic.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# this might largely be meaningless as the setup-alpine gets the bulk of the 
+# work done. 
+
+# grab the standard x86_64 image
+# write it to a usb stick
+# plugin and power up
+# login as root
+
+setup-alpine
+
+# choose the usual
