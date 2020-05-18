d417a5bb5b5ea6f0a88fc94628cd22c9da3e2b40
Andrew Sikes <40582475+umyemri@users.noreply.github.com>
2020-05-18 12:40:01 -0700

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -11,3 +11,30 @@
 setup-alpine
 
 # choose the usual
+# reboot
+# change the repo to add community
+
+apk update
+
+# add the usual
+apk add sudo vim htop tmux ranger feh wireguard ip6tables
+visudo
+adduser name wheel
+su name
+mkdir tl dl dx px vx ax mt
+sudo setup-xorg-base
+sudo apk add git make gcc g++ libx11-dev libxft-dev libxinerama-dev ncurses dbus-x11 firefox-esr adwaita-gtk2-theme adwaita-icon-theme ttf-dejavu
+cd tl
+git clone https://git.suckless.org/dwm
+git clone https://git.suckless.org/dmenu
+git clone https://git.suckless.org/st
+#make edits as you see fit
+cd dwm
+sudo make clean install
+cd ../dmenu
+sudo make clean install
+cd ../st
+sudo make clean install
+echo "feh --bg-fill ~/px/walls/wall.png &" > ~/.xinitrc
+echo "exec dwm" >> ~/.xinitrc
+
