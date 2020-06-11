e1662b0752930a91bd563d7205fd1bfcf4de9ca4
Andrew Sikes <40582475+umyemri@users.noreply.github.com>
2020-06-11 14:27:58 -0700

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -2,19 +2,31 @@
 #
 # post install scripts
 #
+# this is only the most basic of setup. any configuration there after is on you.
+# this assumes you have a wifi.conf made for wpa_supplicant
 
-visudo
-adduser $name wheel
-su $name
+# user setup
+wpa_supplicant -i wlan0 -c wifi.conf -B
+dhcpcd -i wlan0
+read -p 'user: ' uname
+read -sp 'pass: ' upass
+adduser $uname -p $upass
+echo 'myuser ALL=(ALL) NOPASSWD: ALL' >> ./$uname
+mv ./myuser /etc/sudoers.d/
+su $uname
 cd ~
-mkdir tl dl dx px vx ax mt
+
+# make basic folder structure - these are my preferences
+mkdir tl dl dx px vx ax mt .config
+
+# dwm setup from source
 sudo setup-xorg-base
 sudo apk add git make gcc g++ libx11-dev libxft-dev libxinerama-dev dbus-x11 firefox adwaita-gtk2-theme adwaita-icon-theme ttf-dejavu
 cd tl
 git clone https://git.suckless.org/dwm
 git clone https://git.suckless.org/dmenu
 git clone https://git.suckless.org/st
-#make edits as you see fit
+git clone https://git.suckless.org/dwmstatus
 cd dwm
 sudo make clean install
 cd ../dmenu
@@ -23,3 +35,11 @@ cd ../st
 sudo make clean install
 echo "feh --bg-fill ~/px/walls/wall.png &" > ~/.xinitrc
 echo "exec dwm" >> ~/.xinitrc
+
+# install anything else you want at start up.
+sudo apk add dash ranger w3m feh sxiv p7zip dosfstools exfat-utils wireguard ip6tables libxinerama xrandr python3 py3-pip
+
+# done
+echo 'post install complete.'
+echo 'this is a very basic setup. anything you might want to add is on you.'
+echo 'good luck!'
