1e55ebf572eeef157499f4bc89e81262f0f07b4d
Andrew Sikes <40582475+umyemri@users.noreply.github.com>
2020-06-15 14:14:00 -0700

    Update encrypt.sh

--- encrypt.sh
+++ encrypt.sh
@@ -12,13 +12,23 @@
 #
 
 read -p 'host: ' hname
+setup-keymap us us
+setup-hostname $hname
 echo 'auto eth0' > /etc/network/interfaces
 echo 'iface eth0 inet dhcp' >> /etc/network/interfaces
 ifup eth0
+setup-timezone -z US/Pacific
+rc-update add networking boot
+rc-update add urandom boot
+rc-update add acpid default
+rc-service acpid start
+sed -i "s/localhost localhost.localdomain/$hname $hname.localdomain localhost localhost.localdomain" /etc/hosts
 echo 'http://nl.alpinelinux.org/alpine/edge/main' >> /etc/apk/repositories
 echo 'http://nl.alpinelinux.org/alpine/edge/community' >> /etc/apk/repositories
 apk update && apk upgrade
-apk add cryptsetup sgdisk lvm2 dmcrypt boot mkdir # yeah... gnu but that {} method
+apk add cryptsetup sgdisk lvm2 e2fsprogs dosfstools haveged
+rc-service haveged start
+haveged -n 0 | dd of=/dev/sda
 
 # disk partitioning
 sgdisk -og /dev/sda
@@ -29,13 +39,13 @@ sgdisk -n 2:$start_of:$end_of -t 2:8e00 /dev/sda
 sgdisk -p /dev/sda
 
 # luks lvm
-cryptsetup luksFormat /dev/sda2
-cryptsetup open --type luks /dev/sda2 lvm
-pvcreate --dataalignment 1m /dev/mapper/lvm
-vgcreate volume /dev/mapper/lvm
-lvcreate -L 20GB volume -n root
-lvcreate -L 12GB volume -n swap # might be excessive?
-lvcreate -l 100%FREE volume -n home
+cryptsetup -v -c serpent-xts-plain64 -s 512 --hash whirlpool --iter-time 5000 --use-random luksFormat /dev/sda2
+cryptsetup luksOpen /dev/sda2 crypt
+pvcreate --dataalignment 1m /dev/mapper/crypt
+vgcreate volume /dev/mapper/crypt
+lvcreate -L 2G volume -n boot
+lvcreate -L 12GB volume -n swap 
+lvcreate -l 100%FREE volume -n root
 modprobe dm_mod
 modprobe dm_crypt
 vgscan
@@ -44,36 +54,19 @@ vgchange -ay
 # vg file system
 mkfs.fat -F32 /dev/sda1
 mkfs.ext4 /dev/volume/root
-mkfs.ext4 /dev/volume/home
+mkfs.ext4 /dev/volume/boot
 mkswap /dev/volume/swap && swapon /dev/volume/swap
-mount /dev/volume/root /mnt
-mkdir /mnt/{boot,home}
-mount /dev/sda1 /mnt/boot
-mount /dev/volume/home /mnt/home
+mount -t ext4 /dev/volume/root /mnt
+mkdir /mnt/boot
+mount -t ext4 /dev/volume/boot /mnt/boot
+mkdir /mnt/boot/efi
+mount -t vfat /dev/sda1 /mnt/boot/efi
 
 # alpine installation
-apk add --root=/mnt/ --initdb $(cat /etc/apk/world)
-# add edge repo to /etc/apk/repositories & /mnt/root/etc/apk/repositories
-apk add --root=/mnt/ chrony wireless-tools wpa_supplicant
-apk add --root=/mnt/ grub mkinitfs e2fsprogs grub-bios grub-efi
-apk add --root=/mnt/ dosfstools exfat-utils ntfs-3g
-apk add --root=/mnt/ sudo vim tmux gotop ncurses ncdu
-apk add --root=/mnt/ linux util-linux man man-pages mkdir
-mount --bind /dev /mnt/dev
-mount --bind /sys /mnt/sys
-mount --bind /proc /mnt/proc
+setup-disk -m sys /mnt
+mount -t proc /proc /mnt/proc
+mount --rbind /dev /mnt/dev
+mount --make-rslave /mnt/dev
+mount --rbind /sys /mnt/sys
 
-# setup names, times, langs
-chroot /mnt ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
-chroot /mnt hwclock --systohc --utc
-echo "en_US.UTF-8 UTF-8  " >> /mnt/etc/locale.gen
-echo "ja_JP.UTF-8 UTF-8  " >> /mnt/etc/locale.gen
-chroot /mnt locale-gen
-echo "$hname" > /mnt/etc/hostname # finally using that variable /phew
-chroot /mnt hostname -F /etc/hostname
-
-# grub installation
-chroot /mnt grub-install /dev/sda1
-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
-echo 'done'
-echo 'some manual configuration of the grub file will be needed.'
+echo 'done. some manual configuration of the grub / mkinitfs will be needed before rebooting.'
